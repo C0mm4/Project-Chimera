a9cb257d17f2d84755271a4989829edc4d5815e7
C0mm4 <comonhuman@gmail.com>
2025-09-23 09:56:01 +0900

    Init

---
+++ Assets/01 Scripts/Manager/ObjectPoolManager.cs
@@ -0,0 +1,125 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManager : Singleton<ObjectPoolManager>
+{
+    private class SavePool
+    {
+        public string prefabPath;
+        public Transform defaultTransform;
+    }
+
+    private Dictionary<string, Stack<GameObject>> pool = new Dictionary<string, Stack<GameObject>>();
+    private Dictionary<string, SavePool> poolOther = new Dictionary<string, SavePool>();
+
+    //풀 생성
+    public void CreatePool(string _keyValue, string _prefabPath, int _count = 1, Transform _defaultTransform = null)
+    {
+        if (pool.ContainsKey(_keyValue))
+        {
+            //이미 키값으로 생성된게 있음
+            return;
+        }
+
+        Stack<GameObject> queue = new Stack<GameObject>();
+
+        for (int i = 0; i < _count; i++)
+        {
+            //로드+생성 후 오브젝트 끄기
+            GameObject obj = ResourceManager.Instance.Create<GameObject>(_prefabPath,_defaultTransform);
+            obj.SetActive(false);
+
+            //Hierarchy창에서 생성위치 지정
+            if(_defaultTransform != null)
+                obj.transform.SetParent(_defaultTransform,false);
+            
+            //queue에 겜오브젝트 추가
+            queue.Push(obj);
+        }
+
+        pool.Add(_keyValue, queue);
+
+        //ㅇ
+        poolOther[_keyValue] = new SavePool
+        {
+            prefabPath = _prefabPath,
+            defaultTransform = _defaultTransform
+        };
+
+    }
+
+    //풀에서 오브젝트 가져오기
+    public GameObject GetPool(string keyValue)
+    {
+        
+
+        if (!pool.ContainsKey(keyValue))
+        {
+            //키값 없음
+            return null;
+        }
+
+        Stack<GameObject> queue = pool[keyValue];
+
+        if (queue.Count == 0)
+        {
+            //비어있으면 재생성, 저장된 키값에 정보확인
+            if (!poolOther.ContainsKey(keyValue))
+            {
+                return null;
+            }
+
+            //SavePool 클래스에 저장된 부모위치, 프리팹Path불러온거 적용
+            SavePool info = poolOther[keyValue];
+            GameObject obj = ResourceManager.Instance.Create<GameObject>(info.prefabPath, info.defaultTransform);
+            obj.SetActive(false);
+
+            //부모 위치 있는지 확인
+            if (info.defaultTransform != null)
+                obj.transform.SetParent(info.defaultTransform, false);
+
+            //queue 에 생성한 오브젝트 집어넣기
+            queue.Push(obj);
+        }
+
+        //오브젝트 활성화 및 반환 (새로 생성했으면 바로 반환)
+        GameObject otherObj = queue.Pop();
+        otherObj.SetActive(true);
+        return otherObj;
+    }
+
+    //풀에다가 오브젝트 다시 집어넣기
+    public void ResivePool(string keyValue, GameObject obj)
+    {
+        if (!pool.ContainsKey(keyValue))
+        {
+            //키값 없음 , 오브젝트 파괴
+            Destroy(obj);
+            return;
+        }
+
+        //오브젝트 비활성화 및 다시 키값에 반납
+        obj.SetActive(false);
+        obj.transform.SetParent(poolOther[keyValue].defaultTransform, false);
+        pool[keyValue].Push(obj);
+    }
+
+    //풀 삭제
+    public void ClearPool(string keyValue)
+    {
+        if (!pool.ContainsKey(keyValue))
+        {
+            //키값 없음 구?지?
+            return;
+        }
+
+        //오브젝트 삭제
+        foreach (GameObject obj in pool[keyValue])
+        {
+            Destroy(obj);
+        }
+
+        //풀안의 키값 삭제
+        pool.Remove(keyValue);
+    }
+}
---
+++ Assets/01 Scripts/Manager/SoundManager.cs
@@ -0,0 +1,180 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections.Generic;
+using System.Collections;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    [Header("Audio Mixer")]
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private string masterVolumeParam = "MasterVolume";
+    [SerializeField] private string bgmVolumeParam = "BGMVolume";
+    [SerializeField] private string sfxVolumeParam = "SFXVolume";
+
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource bgmSource;
+
+
+    private readonly string MixerPath = "AudioMixer";
+
+    public enum VolumeType
+    {
+        Master, Music, SFX
+    }
+
+    public float masterVolume { get; private set; }
+    public float bgmVolume { get; private set; }
+    public float sfxVolume { get; private set; }    
+    private float SetMasterVolume(float volume) => masterVolume = Mathf.Log10(Mathf.Clamp01(volume)) * 20f;
+    private float SetBGMVolume(float volume) => bgmVolume = Mathf.Log10(Mathf.Clamp01(volume)) * 20f;
+    private float SetSFXVolume(float volume) => sfxVolume = Mathf.Log10(Mathf.Clamp01(volume)) * 20f;
+
+
+    protected void Awake()
+    {
+        audioMixer = ResourceManager.Instance.Load<AudioMixer>(MixerPath);
+
+        ObjectPoolManager.Instance.CreatePool("Pref_SFX", "Pref_SFX", 10, transform);
+    }
+
+    public void SetVolume(float volume, VolumeType type = VolumeType.Master)
+    {
+        switch (type)
+        {
+            case VolumeType.Master:
+                SetMasterVolume(volume);
+                break;
+            case VolumeType.Music:
+                SetBGMVolume(volume);
+                break;
+            case VolumeType.SFX:
+                SetSFXVolume(volume);
+                break;
+        }
+
+        audioMixer.SetFloat(masterVolumeParam, masterVolume);
+        audioMixer.SetFloat(bgmVolumeParam, bgmVolume);
+        audioMixer.SetFloat(sfxVolumeParam, sfxVolume);
+
+    }
+
+    #region BGM
+
+    public void PlayBGM(string path, bool loop = true, float fadeDuration = 1f)
+    {
+        AudioClip clip = Resources.Load<AudioClip>(path);
+        PlayBGM(clip, loop, fadeDuration);
+    }
+
+    public void PlayBGM(AudioClip newClip, bool loop = true, float fadeDuration = 1f)
+    {
+        if (bgmSource == null)
+        {
+            // 이 부분먼 Resource Manager 사용하게 변경하면 될거 같은데
+            bgmSource = ResourceManager.Instance.Create<GameObject>("Pref_BGM").GetorAddComponent<AudioSource>();
+        }
+
+        if (bgmSource.clip == newClip) return;
+
+        StopAllCoroutines();
+        StartCoroutine(FadeAndSwitchBGM(newClip, loop, fadeDuration));
+    }
+
+    private IEnumerator FadeAndSwitchBGM(AudioClip newClip, bool loop, float fadeDuration)
+    {
+        float currentVolume;
+        audioMixer.GetFloat(bgmVolumeParam, out currentVolume);
+        currentVolume = Mathf.Pow(10, currentVolume / 20f); 
+
+        for (float t = 0; t < fadeDuration; t += Time.unscaledDeltaTime)
+        {
+            float v = Mathf.Lerp(currentVolume, 0f, t / fadeDuration);
+            audioMixer.SetFloat(bgmVolumeParam, Mathf.Log10(Mathf.Max(v, 0.0001f)) * 20f);
+            yield return null;
+        }
+
+        audioMixer.SetFloat(bgmVolumeParam, -80f); 
+
+
+        bgmSource.Stop();
+        bgmSource.clip = newClip;
+        bgmSource.loop = loop;
+        bgmSource.Play();
+
+
+        float targetDesbel = Mathf.Pow(10, bgmVolume / 20);
+        for (float t = 0; t < fadeDuration; t += Time.unscaledDeltaTime)
+        {
+            float v = Mathf.Lerp(0f, targetDesbel, t / fadeDuration);
+            audioMixer.SetFloat(bgmVolumeParam, Mathf.Log10(Mathf.Max(v, 0.0001f)) * 20f);
+            yield return null;
+        }
+
+        audioMixer.SetFloat(bgmVolumeParam, bgmVolume); 
+    }
+
+    public void StopBGM()
+    {
+        bgmSource.Stop();
+    }
+
+
+    #endregion
+
+    #region SFX
+
+    public void PlaySFX(AudioClip clip, float volume = 1f)
+    {
+        AudioSource source = GetAvailableSFXSource();
+        source.clip = clip;
+        source.volume = volume;
+        source.Play();
+        StartCoroutine(DisableWhenDone(source));
+    }
+
+
+    private AudioSource GetAvailableSFXSource()
+    {
+        GameObject obj = ObjectPoolManager.Instance.GetPool("Pref_SFX");
+
+        return obj.GetComponent<AudioSource>();
+
+/*
+        if(sfxQueue.Count > 0)
+        {
+            var ret = sfxQueue.Dequeue();
+            ret.gameObject.SetActive(true);
+            return ret;
+        }
+
+        if (sfxSourcePrefab == null)
+        {
+            sfxSourcePrefab = Resources.Load<GameObject>(SFXPrefPath);
+        }
+        var newSource = Instantiate(sfxSourcePrefab, transform);
+        return newSource.GetComponent<AudioSource>();*/
+    }
+
+    private IEnumerator DisableWhenDone(AudioSource src)
+    {
+        yield return new WaitUntil(() => !src.isPlaying);
+
+        src.clip = null;
+
+        ObjectPoolManager.Instance.ResivePool("Pref_SFX", src.gameObject);
+/*
+        if(sfxQueue.Count <= sfxPoolCount)
+        {
+            sfxQueue.Enqueue(src);
+            src.gameObject.SetActive(false);
+        }
+        else
+        { 
+            Destroy(src.gameObject);
+        }*/
+    }
+
+
+    #endregion
+}
