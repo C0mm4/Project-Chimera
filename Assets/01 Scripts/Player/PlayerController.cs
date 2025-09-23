5fa006ce1a98adca6dcb55baa1a60c6f98e54864
C0mm4 <comonhuman@gmail.com>
2025-09-23 13:52:34 +0900

    Test : 플레이어 바라보는 방향에 따른 이동 로직

--- Assets/01 Scripts/Player/PlayerController.cs
+++ Assets/01 Scripts/Player/PlayerController.cs
@@ -6,7 +6,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public Vector2 TargetVelocity { get; private set; }
+    private Vector2 TargetVelocity;
+    [SerializeField] private float rotationSpeed = 10f; // 회전 속도 조절
 
     [SerializeField] private float MoveSpeed = 5f;
     private bool isDash = false;
@@ -28,12 +29,30 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    [SerializeField] private bool Test1 = false;
+
     private void FixedUpdate()
     {
-        Vector2 movDelta = TargetVelocity * (MoveSpeed * (isDash ? 2 : 1)) * Time.fixedDeltaTime;
+        Vector2 movDelta = TargetVelocity;
+
+        Vector3 forward = transform.forward;
+        Vector2 rotationDir = new Vector2(forward.x, forward.z);
+
+        movDelta *= (isDash ? 2 : 1) * Time.fixedDeltaTime * MoveSpeed;
+
+        // Test Code 바라보는 방향에 맞춰서 이동하는 로직
+        if(Test1)
+            movDelta = Vector2.Dot(movDelta, rotationDir) * rotationDir;
 
         transform.position += new Vector3(movDelta.x, 0, movDelta.y);
-        if(TargetVelocity != Vector2.zero)
-            transform.eulerAngles = new Vector3(0f, Mathf.Atan2(TargetVelocity.x, TargetVelocity.y) * Mathf.Rad2Deg, 0f);
+        
+        // 이동 중일때만 회전 적용
+        if (TargetVelocity != Vector2.zero) 
+        {
+            float targetAngle = Mathf.Atan2(TargetVelocity.x, TargetVelocity.y) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+        }
     }
 }
