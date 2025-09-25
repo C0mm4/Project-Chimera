64f5472c41dc14c1a2eea1f5e1d71aba7ef1bd63
SMC <tyuui88@gmail.com>
2025-09-25 21:12:25 +0900

    fix : 무기 테스트 수정

--- Assets/01 Scripts/Player/PlayerChangeWeapon.cs
+++ Assets/01 Scripts/Player/PlayerChangeWeapon.cs
@@ -5,18 +5,24 @@ public class PlayerChangeWeapon : MonoBehaviour
     //무기 생성 위치
     [SerializeField] private GameObject weaponPrefab;
 
-
     private void Awake()
     {
         //모든 무기 생성
-        ObjectPoolManager.Instance.CreatePool("Pref_510000", 1, weaponPrefab.transform);
+
+        //검
         ObjectPoolManager.Instance.CreatePool("Pref_500000", 1, weaponPrefab.transform);
+
+        //활
+        ObjectPoolManager.Instance.CreatePool("Pref_510000", 1, weaponPrefab.transform);
+
         //테스트용
-        ChangeWeapon("Pref_500000");
+        ChangeWeapon(WeaponTypes.Sword);
     }
 
-    public void ChangeWeapon(string name)
+    public void ChangeWeapon(WeaponTypes name)
     {
+        string keyValue = SelectWeapon(name);
+
         //무기 바꾸기전 다 반환
         foreach (Transform transforms in weaponPrefab.transform)
         {
@@ -30,12 +36,27 @@ public class PlayerChangeWeapon : MonoBehaviour
         if (playerAttack != null)
         {
             //name의 프리팹 무기 오브젝트를 활성화
-            GameObject weaponGameobject = ObjectPoolManager.Instance.GetPool(name);
+            GameObject weaponGameobject = ObjectPoolManager.Instance.GetPool(keyValue);
 
             //무기.. weapon으로 되어있어서 일단 넣 모르겠다
             BaseWeapon changeWeapon = weaponGameobject.GetComponent<BaseWeapon>();
             playerAttack.SetWeapon(changeWeapon);
         }
+    }
+
+    private string SelectWeapon(WeaponTypes types)
+    {
+        string keyValue = null;
+
+        //추후 무기 더 생기면 넣어야함
+        switch (types)
+        {
+            case WeaponTypes.Sword:
+                return "Pref_500000";
+            case WeaponTypes.Bow:
+                return "Pref_510000";
+        }    
+        return keyValue;
     }
 }
 
