17943c9e1a83f1b5a6abcd021ca2c7137d6db775
Hira7388 <mine7388@gmail.com>
2025-09-25 17:39:18 +0900

    [Fix] 검 무기 공격 방식

--- Assets/01 Scripts/PlayerAttack/Weapon/Sword.cs
+++ Assets/01 Scripts/PlayerAttack/Weapon/Sword.cs
@@ -14,12 +14,15 @@ public class Sword : BaseWeapon
     {
         base.Awake();
 
-        if (attackHitbox != null)
+        if (hitbox == null)
         {
-            attackHitbox.enabled = false;
+            Debug.LogError("Sword: Hitbox가 연결되지 않았습니다! Hitbox 자식 오브젝트를 확인하세요.", this);
         }
-
-        hitbox = GetComponentInChildren<SwordHitbox>();
+        else if (hitbox.GetComponent<Collider>() != null)
+        {
+            hitbox.GetComponent<Collider>().enabled = false; // 시작 시 반드시 꺼두기
+        }
+        // Todo: 플레이어 애니메이터를 가져오는 로직
     }
 
     protected override void Start()
@@ -32,12 +35,15 @@ public class Sword : BaseWeapon
     {
         // 공격 방향으로 플레이어(또는 무기)의 방향을 돌립니다.
         transform.root.LookAt(target); // transform.root는 플레이어 전체를 의미
+        transform.root.eulerAngles = new Vector3(0, transform.root.eulerAngles.y, 0);
 
-        hitbox.StartAttack(MeleeData.Damage);
         // Todo: 애니메이션 시작, 검기 VFX 추가, 사운드 추가
 
         // 히트박스를 잠시 켰다 끄는 코루틴을 시작합니다.
-        StartCoroutine(AttackHitboxRoutine());
+        if (hitbox != null)
+        {
+            StartCoroutine(AttackHitboxRoutine());
+        }
     }
 
     // 히트박스 공격용 루틴
@@ -45,17 +51,18 @@ public class Sword : BaseWeapon
     {
         Debug.Log("검 공격 시작!");
 
+        hitbox.StartAttack(MeleeData.Damage);
         // 히트박스를 켭니다.
-        attackHitbox.enabled = true;
+        hitbox.enabled = true;
 
         // 휘두르기 모션? 등을 추가
 
         // 0.3초 동안 공격 판정을 유지합니다.
         // melee SO에 추가해도 좋을 것 같습니다.
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(MeleeData.hitboxActiveDuration);
 
         // 히트박스를 다시 끕니다.
-        attackHitbox.enabled = false;
+        hitbox.enabled = false;
 
         Debug.Log("검 공격 종료!");
     }
