8841e2e92510224809aad52259e2dfc10007e5a9
alksf128 <65576878+alksf128@users.noreply.github.com>
2025-09-29 19:25:46 +0900

    fix/골드 관련

--- Assets/01 Scripts/Item/DroppedItemController.cs
+++ Assets/01 Scripts/Item/DroppedItemController.cs
@@ -5,25 +5,26 @@ using UnityEngine;
 public class DroppedItemController : MonoBehaviour
 {
     private Rigidbody rb;
+    private Collider col;
     private bool hasLanded = false;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(hasLanded)
-        {
-            return;
-        }
+        if(hasLanded) return;
 
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
             rb.isKinematic = true;
+            hasLanded = true; // 물리 움직임 정지
 
-            hasLanded = true;
+            col.isTrigger = true; // 바닥에 닿으면 트리거로 변경
         }
     }
 }
