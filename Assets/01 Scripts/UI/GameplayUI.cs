0e1cd80203bd434d807674cb00c9dbb61b392946
Hira7388 <mine7388@gmail.com>
2025-09-30 19:06:47 +0900

    [Feat] GameplayUI 그룹화 변경

--- Assets/01 Scripts/UI/GameplayUI.cs
+++ Assets/01 Scripts/UI/GameplayUI.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 
 public class GameplayUI : UIBase
 {
+    [Header("UI 그룹")]
+    // 게임 플레이 UI 요소 전체를 담는 부모 오브젝트
+    [SerializeField] private GameObject hudGroup;
+
     [Header("UI 요소 연결")]
     [SerializeField] private Button pauseButton;
     [SerializeField] private TextMeshProUGUI goldText;
@@ -33,21 +37,8 @@ public class GameplayUI : UIBase
 
     private void Update()
     {
-        // 팝업 UI가 열려있으면 일시정지 버튼을 숨김
-        if (UIManager.Instance.PopupStackCount > 0)
-        {
-            if (pauseButton.gameObject.activeSelf)
-            {
-                pauseButton.gameObject.SetActive(false);
-            }
-        }
-        else
-        {
-            if (!pauseButton.gameObject.activeSelf)
-            {
-                pauseButton.gameObject.SetActive(true);
-            }
-        }
+        // 팝업 UI가 비어있을 때만 HUD 그룹을 활성화
+        hudGroup.SetActive(UIManager.Instance.PopupStackCount == 0);
     }
 
     private void OnPauseButtonClicked()
